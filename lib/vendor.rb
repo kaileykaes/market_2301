7aac48295af048e761084e53cc50b58f4983fa4c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 09:40:47 -0700

    feat: #check_stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -6,4 +6,13 @@ class Vendor
     @name = name
     @inventory = Hash.new(0)
   end
+
+  def check_stock(item)
+      if @inventory.include?(item)
+        return @inventory[item]
+      else
+        @inventory[item] = 0
+      end
+  end 
+
 end
