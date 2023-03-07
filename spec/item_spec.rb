0ee45b7b5fa2a67d16104cbac9131fcc24e67cd4
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 09:09:20 -0700

    Test: exists and has attributes

---
+++ spec/item_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new({
+      name: 'Peach', 
+      price: "$0.75
+      "})
+    @item2 = Item.new({
+      name: 'Tomato', 
+      price: '$0.50'
+      })
+  end
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+  end
+
+  it 'has attributes' do
+    expect(@item2.name).to eq('Tomato')
+    expect(@item2.price).to eq(0.5)
+  end
+end
