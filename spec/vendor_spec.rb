6980020d9985d2d2a08db1398c5e9f09136da360
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 09:18:34 -0700

    test: exists

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @item1 = Item.new({
+      name: 'Peach', 
+      price: "$0.75"})
+    @item2 = Item.new({
+      name: 'Tomato', 
+      price: '$0.50'
+      })
+    @vendor = Vendor.new("Rocky Mountain Fresh")  
+  end
+
+  it 'exists' do
+    expect(@vendor).to be_a(Vendor)
+  end
+end
