758ab605e49bc9b205a7523c56a4eaad491a638a
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 10:46:29 -0700

    xit - it

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -37,21 +37,21 @@ RSpec.describe Market do
     expect(@market.vendors).to eq([])
   end
 
-  xit 'can add vendors' do
+  it 'can add vendors' do
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
     expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
   end
 
-  xit 'can return vendor names' do
+  it 'can return vendor names' do
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
     expect(@market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
   end
   
-  xit 'can find vendors that sell items' do
+  it 'can find vendors that sell items' do
     @market.add_vendor(@vendor1)
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
