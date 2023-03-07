840e22cdc6b616218996168d93ca3284c3e4cea6
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-07 09:15:04 -0700

    refactor: test string not float

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Item do
   before(:each) do
     @item1 = Item.new({
       name: 'Peach', 
-      price: "$0.75
-      "})
+      price: "$0.75"})
     @item2 = Item.new({
       name: 'Tomato', 
       price: '$0.50'
@@ -18,6 +17,6 @@ RSpec.describe Item do
 
   it 'has attributes' do
     expect(@item2.name).to eq('Tomato')
-    expect(@item2.price).to eq(0.5)
+    expect(@item2.price).to eq('$0.50')
   end
 end
